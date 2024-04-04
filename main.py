29ad1ab03ffd2593cb1e16fb5fb3f62c3c4a52e2
Ilia Kustov <36426291+ikustow@users.noreply.github.com>
2024-04-04 23:53:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,11 +18,12 @@ app = FastAPI()
 class CreateMessgeRequest(BaseModel):
     mnemonic: str
     key: str
+    new_owner_address: str
+    to_addr: str
 
 @app.post("/send_message")
 def send_message(request: CreateMessgeRequest):
-    key= b'2GCELT6Qssd7fNbNsu8hjD9-pswwTItZZ4_W7zDlE6o='
-
+    key= request.key.encode() 
     mnemonic =  request.mnemonic.encode() 
  
     decrypted_mnemonic = decrypt_strings(mnemonic, key)
@@ -34,8 +35,8 @@ def send_message(request: CreateMessgeRequest):
         mnemonics, pub_k, priv_k, wallet = Wallets.from_mnemonics(mnemonics=decrypted_mnemonic, version=WalletVersionEnum.v4r2, workchain=0)
 
         # Transfer logic
-        body = NFTItem().create_transfer_body(new_owner_address=Address('UQD4452NIi3YSL4t7ECbPfzH5f3BsHXWltPkF19nNshL9bkB'))
-        query = wallet.create_transfer_message(to_addr='EQBoHNzacteIGIeLVn7Fy4BO-RIQeQeMEvAN5UAlrxYDTH35',
+        body = NFTItem().create_transfer_body(new_owner_address=Address(request.new_owner_address))
+        query = wallet.create_transfer_message(to_addr=request.to_addr,
                                                amount=to_nano(0.001, 'ton'),
                                                seqno=int(0x4),
                                                payload=body)
