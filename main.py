6e6b207fe2e5383376406c15905b6a282ba349d8
Ilia Kustov <36426291+ikustow@users.noreply.github.com>
2024-04-05 00:05:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import base64
+import requests
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from tonsdk.contract.token.nft import NFTItem
@@ -6,47 +7,50 @@ from tonsdk.utils import to_nano, bytes_to_b64str, Address
 from tonsdk.contract.wallet import Wallets, WalletVersionEnum
 from cryptography.fernet import Fernet
 
-
-def decrypt_strings(encrypted_message, key):
-    fernet = Fernet(key)
-    decrypted_message = fernet.decrypt(encrypted_message).decode()
-    strings = decrypted_message.split("\n")  # Разделяем строку обратно на массив строк
-    return strings
-
 app = FastAPI()
 
-class CreateMessgeRequest(BaseModel):
+class CreateMessageRequest(BaseModel):
     mnemonic: str
     key: str
     new_owner_address: str
     to_addr: str
 
+def decrypt_strings(encrypted_message, key):
+    fernet = Fernet(key)
+    decrypted_message = fernet.decrypt(encrypted_message).decode()
+    return decrypted_message.split("\n")
+
 @app.post("/send_message")
-def send_message(request: CreateMessgeRequest):
-    key= request.key.encode() 
-    mnemonic =  request.mnemonic.encode() 
- 
-    decrypted_mnemonic = decrypt_strings(mnemonic, key)
- 
-    if mnemonic == '':
+def send_message(request: CreateMessageRequest):
+    key = base64.b64decode(request.key)
+    mnemonic = base64.b64decode(request.mnemonic)
+
+    if not mnemonic:
         raise HTTPException(status_code=400, detail="Mnemonic cannot be empty")
 
     try:
-        mnemonics, pub_k, priv_k, wallet = Wallets.from_mnemonics(mnemonics=decrypted_mnemonic, version=WalletVersionEnum.v4r2, workchain=0)
+        decrypted_mnemonic = decrypt_strings(mnemonic, key)
+        mnemonics, pub_k, priv_k, wallet = Wallets.from_mnemonics(
+            mnemonics=decrypted_mnemonic[0], 
+            version=WalletVersionEnum.v4r2, 
+            workchain=0
+        )
 
         # Transfer logic
         body = NFTItem().create_transfer_body(new_owner_address=Address(request.new_owner_address))
-        query = wallet.create_transfer_message(to_addr=request.to_addr,
-                                               amount=to_nano(0.001, 'ton'),
-                                               seqno=int(0x4),
-                                               payload=body)
+        query = wallet.create_transfer_message(
+            to_addr=request.to_addr,
+            amount=to_nano(0.001, 'ton'),
+            seqno=int(0x4),
+            payload=body
+        )
 
-        # Convert the message to BOC and print (or return) it
         boc = bytes_to_b64str(query["message"].to_boc(False))
-        
+        url = 'https://toncenter.com/api/v2/sendBoc'
+        data = {"boc": boc}
+        response = requests.post(url, json=data)
 
-        return {"message"}
+        return {"message": "Success", "response": response.text}
 
     except Exception as e:
-        # Handle other exceptions or errors
         raise HTTPException(status_code=500, detail=str(e))
