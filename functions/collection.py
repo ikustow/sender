6cbffc4773caa77902baf7f3d0bd375e0f0b4c32
ikust <ikustov.dev@gmail.com>
2024-05-15 00:01:45 +0200

    ref

--- functions/collection.py
+++ functions/collection.py
@@ -4,11 +4,8 @@ from TonTools import *
 
 COLLECTION = 'EQBs85otZAYdYQNSUEUWg_C7DnKERAFvPaJtKcKlI9Po8S0e'
 
-<<<<<<< HEAD
 async def get_collection():
-=======
-async def main():
->>>>>>> 1f9a72f45e30fd298dedad70a9be35902e577e5a
+
     client = TonCenterClient(orbs_access=True)
 
     data = await client.get_collection(collection_address=COLLECTION)
@@ -20,10 +17,6 @@ async def main():
         data1 = await client.get_nft_items(nft_addresses=[item.address])
 
         print(data1[0]) 
-  
-
-<<<<<<< HEAD
+ 
 asyncio.run(get_collection())
-=======
-asyncio.run(main())
->>>>>>> 1f9a72f45e30fd298dedad70a9be35902e577e5a
+
