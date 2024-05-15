79f29aa73a1bd0995c2c5bd7382a324fb2dc20f4
ikust <ikustov.dev@gmail.com>
2024-05-16 00:04:48 +0200

    update

--- main.py
+++ main.py
@@ -10,14 +10,49 @@ from tonsdk.utils import to_nano, bytes_to_b64str, Address
 from tonsdk.contract.wallet import Wallets, WalletVersionEnum
 from cryptography.fernet import Fernet
 from functions.collection import parse_response
-from functions.sending import send_message
 from model.CreateMessageRequestClass import CreateMessageRequest
+
 COLLECTION = 'EQBs85otZAYdYQNSUEUWg_C7DnKERAFvPaJtKcKlI9Po8S0e'
+
+
+
+def decrypt_strings(encrypted_message, key):
+    fernet = Fernet(key)
+    decrypted_message = fernet.decrypt(encrypted_message).decode()
+    return decrypted_message.split("\n")
+
+
 app = FastAPI()
 
 @app.post("/send_message")
-async def handle_send_message(request: CreateMessageRequest):
-    return send_message(request)
+def send_message(request: CreateMessageRequest):
+    key= request.key.encode() 
+    mnemonic =  request.mnemonic.encode() 
+
+    if not mnemonic:
+        raise HTTPException(status_code=400, detail="Mnemonic cannot be empty")
+
+    try:
+        decrypted_mnemonic = decrypt_strings(mnemonic, key)
+        mnemonics, pub_k, priv_k, wallet = Wallets.from_mnemonics(
+            mnemonics=decrypted_mnemonic, 
+            version=WalletVersionEnum.v4r2, 
+            workchain=0
+        )
+        body = NFTItem().create_transfer_body(new_owner_address=Address(request.new_owner_address))
+        query = wallet.create_transfer_message(
+            to_addr=request.to_addr,
+            amount=to_nano(0.01, 'ton'),
+            seqno=int(0x9),
+            payload=body
+        )
+
+        boc = bytes_to_b64str(query["message"].to_boc(False))
+        response = requests.post('https://toncenter.com/api/v2/sendBoc', data= json.dumps({"boc":boc})).json()
+        return {"message": "Success", "response": response}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.get("/collection")
 async def get_collection():
