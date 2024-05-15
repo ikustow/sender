e9b22a479e8573af746516d6fde4428e02c04e16
ikust <ikustov.dev@gmail.com>
2024-05-15 23:02:57 +0200

    ref

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import asyncio
+from TonTools import *
 import base64
 import json
 import requests
@@ -7,8 +9,6 @@ from tonsdk.contract.token.nft import NFTItem
 from tonsdk.utils import to_nano, bytes_to_b64str, Address
 from tonsdk.contract.wallet import Wallets, WalletVersionEnum
 from cryptography.fernet import Fernet
-
-from functions.collection import get_collection
 from functions.sending import send_message
 from model.CreateMessageRequestClass import CreateMessageRequest
 
@@ -19,5 +19,17 @@ async def handle_send_message(request: CreateMessageRequest):
     return send_message(request)
 
 @app.get("/collection")
-async def handle_get_collection():
-    return get_collection()
+async def get_collection():
+    COLLECTION = 'EQBs85otZAYdYQNSUEUWg_C7DnKERAFvPaJtKcKlI9Po8S0e'
+    client = TonCenterClient(orbs_access=True)
+
+    data = await client.get_collection(collection_address=COLLECTION)
+    items = await client.get_collection_items(collection=data, limit_per_one_request=20)
+
+    for item in items:
+        #print(item.address)
+        
+        data1 = await client.get_nft_items(nft_addresses=[item.address])
+
+        #print(data1[0]) 
+    return {"message": "Success", "response": "$data"}
